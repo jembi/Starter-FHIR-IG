0cbb1c768b5def6b2f23b882bec4b2b270601680
rmrlangford <richard.langford@jembi.org>
2024-12-15 17:21:55 +0200

    updated

---
+++ local-template/package/content/assets/js/header.js
@@ -0,0 +1,7 @@
+var familyLogoHyperlink = document.querySelectorAll("#family-logo")[0];
+familyLogoHyperlink.href = "https://www.jembi.org/";
+
+var familyLogoImage = document.querySelectorAll("#family-logo img")[0];
+familyLogoImage.src = "./../{{site.data.info.assets}}assets/images/organization.png";
+familyLogoImage.height = "70";
+familyLogoImage.alt = "Jembi website";
